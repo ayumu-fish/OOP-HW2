0dcc383341858b0a733d92e19ad8986e7a5b0294
fcu-D0486437 <d0486437@fcu.edu.tw>
2016-10-14 10:03:41 +0800

    修改迴圈

--- StringComparator.java
+++ StringComparator.java
@@ -4,15 +4,18 @@ public class StringComparator {
 		public static void main(String[] args){
 		Scanner cin=new Scanner(System.in);
 		String s1,s2; 
+		int result=0;
+		do
+		{
+			System.out.printf("Enter a string 1:");
+			s1=cin.next();
+			System.out.printf("Enter a string 2:");
+			s2=cin.next();
+			result=s1.compareToIgnoreCase(s2);
+			if(result!=0) System.out.printf("The two strings are not the same"); 
+		}while(result!=0);
 		
-		System.out.printf("Enter a string 1:");
-		s1=cin.next();
-		System.out.printf("Enter a string 2:");
-		s2=cin.next();	
-		
-		if(s1.compareToIgnoreCase(s2)==0) System.out.printf("The two strings are the same");
-		else System.out.printf("The two strings are not the same");
-		
+		System.out.printf("The two strings are the same");
 		cin.close();
 		}
 }
